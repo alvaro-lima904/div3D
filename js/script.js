d37a1c91c214efa51bd1b276d02ddbb9f523e64c
Alvaro Carvalho de Lima <112179805+Asano044@users.noreply.github.com>
2022-11-07 02:59:45 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $('body').mousemove(function(p) {
-    let rotateX = -($(window).innerHeight()/2-p.pageY)/-10;
+    let rotateX =  -($(window).innerHeight()/2-p.pageY)/-10;
     let rotateY =  -($(window).innerWidth()/2-p.pageX)/20;
     $('.card').css("transform", "rotateX("+rotateX+"deg) rotateY("+rotateY+"deg)");
 });
